bcc43f1f852f49646aaff090a0d311e96d56dc74
Cristian Echevarria <99057175+Echxvx2610@users.noreply.github.com>
2025-02-10 06:17:17 -0800

    update
    creacion de carpeta

---
+++ bucles e iteracion/script.js
@@ -0,0 +1,11 @@
+// Seleccionamos el contenedor
+let contenido = document.getElementById("container");
+
+// Creamos un nuevo párrafo
+let nuevoParrafo = document.createElement("p");
+
+// Asignamos el texto al nuevo párrafo
+nuevoParrafo.textContent = "Bucles ( while & do while )";
+
+// Añadimos el párrafo al contenedor
+contenido.appendChild(nuevoParrafo);
